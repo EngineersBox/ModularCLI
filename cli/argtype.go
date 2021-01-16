68d174898b7e0f3aa3f5d00d1aecbf6677b9e6d2
u6940136@anu.edu.au <toastcraft.info@gmail.com>
2021-01-17 02:42:31 +1100

    Moved arg declarations to Argument.go from argtype.go

--- cli/argtype.go
+++ cli/argtype.go
@@ -1,91 +1,11 @@
 package cli
 
-import (
-	"os"
-	"strings"
-)
-
 type ArgType int
 
 const (
 	TypeInvalid ArgType = iota
 	TypeInt
+	TypeFloat
 	TypeBool
 	TypeString
 )
-
-type Argument struct {
-	Type         ArgType
-	Name         string
-	DefaultValue interface{}
-	Required     bool
-	HelpMsg      string
-	ValidateFunc ArgValidateFunc
-	Value        *interface{}
-	TypedArgument
-}
-
-type ArgValidateFunc func(TypedArgument) error
-
-func (a Argument) IsPresent() bool {
-	var present = false
-	for _, arg := range os.Args {
-		present = present || strings.Contains(arg, "--"+a.Name)
-	}
-	return present
-}
-
-type TypedArgument interface {
-	GetString() *string
-	GetBool() *bool
-	GetInt() *int
-	GetArg() *Argument
-}
-
-type StringArgument struct {
-	Value *string
-	*Argument
-}
-
-func (s StringArgument) GetString() *string {
-	return s.Value
-}
-
-func (StringArgument) GetBool() *bool { return nil }
-func (StringArgument) GetInt() *int   { return nil }
-
-func (s StringArgument) GetArg() *Argument {
-	return s.Argument
-}
-
-type IntArgument struct {
-	Value *int
-	*Argument
-}
-
-func (i IntArgument) GetInt() *int {
-	return i.Value
-}
-
-func (IntArgument) GetBool() *bool     { return nil }
-func (IntArgument) GetString() *string { return nil }
-
-func (i IntArgument) GetArg() *Argument {
-	return i.Argument
-}
-
-type BoolArgument struct {
-	Value *bool
-	*Argument
-}
-
-func (b BoolArgument) GetBool() *bool {
-	return b.Value
-}
-
-func (BoolArgument) GetString() *string { return nil }
-func (BoolArgument) GetInt() *int       { return nil }
-
-func (b BoolArgument) GetArg() *Argument {
-	return b.Argument
-}
